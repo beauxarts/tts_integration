e1312eed62b82eb570f2f9e57acc525a67418d49
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2023-01-09 17:38:04 -0800

    Fixing say Pause

--- say/synthesizer.go
+++ say/synthesizer.go
@@ -76,5 +76,5 @@ func (s *Synthesizer) IsNameRequired() bool {
 
 func (s *Synthesizer) Pause(dur time.Duration) (string, tts_integration.SynthesisInputType) {
 	return fmt.Sprintf("[[slnc %d]]]", dur.Milliseconds()),
-		tts_integration.SSML
+		tts_integration.Text
 }
